51abb5a5081a8a946116f7d612fec2d3ea5bafb6
new-thanh <new-thanh@eplus.vn>
2020-09-09 13:57:56 +0700

    update API for user CRUD with superAdmin permission

---
+++ controllers/userController.js
@@ -0,0 +1,187 @@
+const Users = require("../models/users");
+const bcrypt = require("bcryptjs");
+
+module.exports = {
+  getListUser: async (req, res) => {
+    try {
+      const { page, perpage, find } = req.query;
+      let skip = perpage * (page - 1);
+      let filter = find ? find : "";
+
+      const users = await Users.find({
+        status: { $ne: "DELETE" },
+        $or: [
+          { username: new RegExp(filter, "i") },
+          {
+            fullname: new RegExp(filter, "i"),
+          },
+        ],
+      })
+        .skip(+skip)
+        .limit(+perpage);
+
+      const total = await Users.find({
+        status: { $ne: "DELETE" },
+        $or: [
+          { username: new RegExp(filter, "i") },
+          {
+            fullname: new RegExp(filter, "i"),
+          },
+        ],
+      }).count();
+
+      return res.status(200).json({
+        status: true,
+        data: {
+          users,
+          page,
+          per_page: perpage,
+          total,
+          total_page: Math.ceil(total / perpage),
+          filter,
+        },
+      });
+    } catch (e) {
+      return res.status(500).json({
+        status: false,
+        data: {
+          message: "There was a problem",
+          e,
+        },
+      });
+    }
+  },
+  update: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { fullname, status, birthday } = req.body;
+
+      const user = await Users.update(
+        { _id: id },
+        { fullname, birthday, status, updateAt: Date.now() }
+      );
+      if (!user)
+        return res.status(500).json({
+          status: false,
+          data: {
+            message: "The was a problem when updating a comment",
+          },
+        });
+      return res.status(200).json({
+        status: true,
+        data: user,
+      });
+    } catch (e) {
+      return res.status(500).json({
+        status: false,
+        data: {
+          message: "There was a problem",
+          e,
+        },
+      });
+    }
+  },
+  delete: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const user = await Users.update({ _id: id }, { status: "DELETE" });
+      if (!user)
+        return res.status(500).json({
+          status: false,
+          data: {
+            message: "The was a problem when deleting a user",
+          },
+        });
+      return res.status(200).json({
+        status: true,
+        data: user,
+      });
+    } catch (e) {
+      return res.status(500).json({
+        status: false,
+        data: {
+          message: "There was a problem",
+          e,
+        },
+      });
+    }
+  },
+  adminChangePassword: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { password } = req.body;
+      const hashedPassword = bcrypt.hashSync(password, 8);
+      const user = await Users.update(
+        { _id: id },
+        {
+          password: hashedPassword,
+          updateAt: Date.now(),
+        }
+      );
+      if (!user)
+        return res.status(500).json({
+          status: false,
+          data: {
+            message: "The was a problem when updating password a user",
+          },
+        });
+      return res.status(200).json({
+        status: true,
+        data: user,
+      });
+    } catch (e) {
+      return res.status(500).json({
+        status: false,
+        data: {
+          message: "There was a problem",
+          e,
+        },
+      });
+    }
+  },
+
+  create: async (req, res) => {
+    try {
+      const { fullname, username, birthday, password } = req.body;
+      const hashedPassword = bcrypt.hashSync(password, 8);
+
+      const checkUserExist = await Users.find({ username }).count();
+      if (checkUserExist) {
+        return res.status(200).json({
+          status: false,
+          data: {
+            message: "The username already exists",
+          }
+        });
+      }
+
+      let usernameTrim = username.replace(/\s/gi,"");
+
+      const user = await Users.create({
+        fullname,
+        username: usernameTrim,
+        password: hashedPassword,
+        birthday,
+      });
+      if (!user)
+        return res.status(500).json({
+          status: false,
+          data: {
+            message: "The was a problem when creating a user",
+          },
+        });
+      return res.status(200).json({
+        status: true,
+        data: user,
+      });
+    } catch (e) {
+      return res.status(500).json({
+        status: false,
+        data: {
+          message: "There was a problem",
+          e,
+        },
+      });
+    }
+  },
+};
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+const userController = require("../controllers/userController");
+const verifyToken = require("../utils/verifyToken");
+const isAdmin = require("../utils/isAdmin");
+const isSuperAdmin = require("../utils/isSuperAdmin");
+
+router.get("/list", [verifyToken, isSuperAdmin], userController.getListUser);
+router.put("/update/:id", [verifyToken, isSuperAdmin], userController.update);
+router.delete(
+  "/delete/:id",
+  [verifyToken, isSuperAdmin],
+  userController.delete
+);
+router.put(
+  "/change_password/:id",
+  [verifyToken, isSuperAdmin],
+  userController.adminChangePassword
+);
+
+router.post("/create", [verifyToken, isSuperAdmin], userController.create);
 
 module.exports = router;
